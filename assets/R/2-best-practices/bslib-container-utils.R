b87409604c09e1a3121b9b70fe72b96b2efd741b
DivadNojnarg <dgranjon@ymail.com>
2023-09-03 22:16:22 +0200

    reorder parts + start to add more instructions for shinyMons2 redesign

---
+++ assets/R/2-best-practices/bslib-container-utils.R
@@ -0,0 +1,31 @@
+describe_layout_function <- function(
+  name = NULL,
+  description = NULL,
+  element = NULL,
+  full_screen = FALSE
+) {
+  if (!is.null(name)) {
+    name <- knitr::combine_words(
+      sprintf("<code>%s</code>", name),
+      and = " or "
+    )
+  }
+
+  if (is.null(element)) {
+    element <- card_body(
+      class = "d-flex flex-column align-items-center",
+      div(style = "width: 6em; height: 6em; background-color: gray")
+    )
+  }
+
+  card(
+    full_screen = full_screen,
+    if (!is.null(name)) {
+      card_header(class = "bg-light text-center", HTML(name))
+    },
+    element,
+    if (!is.null(description)) {
+      card_footer(class = "bg-white text-center", HTML(description))
+    }
+  ) 
+}
